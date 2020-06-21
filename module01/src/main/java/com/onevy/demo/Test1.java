9f42e9f4829775fc051e9c71e91fccc8d73cf2e1
wanfei.zhang <wanfei.zhang@rograndec.com>
2020-06-21 19:31:49 +0800

    Initial commit

---
+++ module01/src/main/java/com/onevy/demo/Test1.java
@@ -0,0 +1,15 @@
+package com.onevy.demo;
+
+/**
+ * @author wanfei.zhang
+ * @create 2020-06-21 17:03
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println(args);
+
+        String[] strings;
+        strings = new String[]{"11111"};
+
+    }
+}
